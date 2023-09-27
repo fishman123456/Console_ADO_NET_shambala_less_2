cf30a3a38fa3614859e86360f4f00617086402ec
fishman123456 <den9152@yandex.ru>
2023-09-28 02:05:33 +0700

    добавил метод update

--- Базовый CRUD ADO.cs
+++ Базовый CRUD ADO.cs
@@ -1,26 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace ConsoleApp1
+namespace FirstDbApp
 {
-
     internal class Program
     {
+        // ЗАДАЧА: 
+        // реализовать базовые CRUD-операции для таблицы БД
+        // в виде процедур (статических методов класса Program)
+        // CRUD - create read update delete
+        // - добавить новую запись в таблицу    +
+        // - получить все записи таблицы        +
+        // - получить запись таблицы по id      +
+        // - изменить запись
+        // - удалить запись
+
         // 1. вспомогательная процедура, создающая и открывающая подключение к БД
         static SqlConnection OpenDbConnection()
         {
             // обработка исключений будет выполняться выше по стеку
-            string connectionString = @"Data Source=fishman\SQLEXPRESS;
-                                    Initial Catalog=computer_game_db;
-                                    Integrated Security=SSPI;";
+            string connectionString = @"Data Source=LAPTOP-SCD1CHJF\SQLEXPRESS;
+                                            Initial Catalog=computer_games_db;
+                                            Integrated Security=SSPI;";
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
             return connection;
         }
+
         // 2. вспомогательная процедура, читающая и выводящая табличный результат запроса (SqlDatareader)
         static void ReadQueryResult(SqlDataReader queryResult)
         {
@@ -31,13 +43,19 @@ namespace ConsoleApp1
             }
             Console.WriteLine(queryResult.GetName(queryResult.FieldCount - 1));
             // 2. вывести значения построчно
+            bool noRows = true;
             while (queryResult.Read())
             {
+                noRows = false;
                 for (int i = 0; i < queryResult.FieldCount - 1; i++)
                 {
                     Console.Write($"{queryResult[i]} - ");
                 }
                 Console.WriteLine(queryResult[queryResult.FieldCount - 1]);
+            }
+            if (noRows)
+            {
+                Console.WriteLine("No rows in result");
             }
         }
 
@@ -56,17 +74,15 @@ namespace ConsoleApp1
                 queryResult = query.ExecuteReader();
                 // 4. считать запрос (универсальный способ)
                 ReadQueryResult(queryResult);
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 Console.WriteLine($"Something wrong: {ex.Message}");
-            }
-            finally
+            } finally
             {
                 connection?.Close();    // закрыть соединение (если != null)
                 queryResult?.Close();
             }
         }
+
         // 4. процедура получения записи по id
         static void SelectRowById(int id)
         {
@@ -93,6 +109,7 @@ namespace ConsoleApp1
                 queryResult?.Close();
             }
         }
+
         // 5. процедура добавления новой записи в таблицу
         static void InsertRow(string name, int released_in, decimal price)
         {
@@ -101,18 +118,17 @@ namespace ConsoleApp1
             {
                 // 1. открыть соединение
                 connection = OpenDbConnection();
-                // 2. подготовить запрос [name_f] ,[released_in_f]   ,[prise_f]
-                string cmdString =
+                // 2. подготовить запрос
+                string cmdString = 
                     $"INSERT INTO game_t (name_f, released_in_f, price_f) VALUES ('{name}', {released_in}, {price});";
                 SqlCommand cmd = new SqlCommand(cmdString, connection);
                 // 3. выполнить запрос
                 int rowsAffected = cmd.ExecuteNonQuery();   // выполнение запроса, изменяющего строки таблицы
-                                                            // 4. проверить результат выполнения
+                // 4. проверить результат выполнения
                 if (rowsAffected != 1)
                 {
                     Console.WriteLine($"INSERT failed, rowsAffected != 1 ({rowsAffected})");
-                }
-                else
+                } else
                 {
                     Console.WriteLine("Successfully inserted");
                 }
@@ -127,7 +143,7 @@ namespace ConsoleApp1
             }
         }
 
-        // 6. процедура удаления записи из таблицы
+        // 6. процедура удаления записи
         static void DeleteRow(int id)
         {
             SqlConnection connection = null;
@@ -137,18 +153,22 @@ namespace ConsoleApp1
                 connection = OpenDbConnection();
                 // 2. подготовить запрос
                 string cmdString =
-                    $"DELETE from game_t where id ={id};";
+                    $"DELETE FROM game_t WHERE id = {id}";
                 SqlCommand cmd = new SqlCommand(cmdString, connection);
                 // 3. выполнить запрос
                 int rowsAffected = cmd.ExecuteNonQuery();   // выполнение запроса, изменяющего строки таблицы
-                                                            // 4. проверить результат выполнения
-                if (rowsAffected != 1)
+                // 4. проверить результат выполнения
+                switch (rowsAffected)
                 {
-                    Console.WriteLine($"DELETE failed, rowsAffected != 1 ({rowsAffected})");
-                }
-                else
-                {
-                    Console.WriteLine("Successfully deleted");
+                    case 0:
+                        Console.WriteLine("No rows matching for delte");
+                        break;
+                    case 1:
+                        Console.WriteLine("Row removed");
+                        break;
+                    default:
+                        Console.WriteLine("DELETE failed, rowsAffected != 0 || rowsAffected != 1");
+                        break;
                 }
             }
             catch (Exception ex)
@@ -160,56 +180,17 @@ namespace ConsoleApp1
                 connection?.Close();    // закрыть соединение (если != null)
             }
         }
-        // 7. процедура обновления записи в таблице
+
+        // 7. процедура обновления записи
         static void UpdateRow(int id, string newName, int newReleasedIn, decimal newPrice)
         {
-            SqlConnection connection = null;
-            try
-            {
-                // 1. открыть соединение
-                connection = OpenDbConnection();
-                // 2. подготовить запрос
-                // [id]
-                // [name_f]
-                // [released_in_f]
-                // [price_f]
-                // FROM[computer_game_db].[dbo].[game_t]
-                string cmdString =
-                    $"update game_t set name_f ='{newName}'," +
-                    $"released_in_f ={newReleasedIn}" +
-                    $"price_f ={newPrice} " +
-                    $"where id ={id};";
-                SqlCommand cmd = new SqlCommand(cmdString, connection);
-                // 3. выполнить запрос
-                int rowsAffected = cmd.ExecuteNonQuery();   // выполнение запроса, изменяющего строки таблицы
-                                                            // 4. проверить результат выполнения
-                if (rowsAffected != 1)
-                {
-                    Console.WriteLine($"Update failed, rowsAffected != 1 ({rowsAffected})");
-                }
-                else
-                {
-                    Console.WriteLine("Successfully update");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Something wrong: {ex.Message}");
-            }
-            finally
-            {
-                connection?.Close();    // закрыть соединение (если != null)
-                Console.WriteLine("Connections close");
-            }
+            
         }
 
-        static void Main(string[] arg)
-        {
-             InsertRow("redalert", 2001, 950);
-            //DeleteRow(6);
-            UpdateRow(9, "redalert", 2001, 950);
-            SelectAllRows();
-
-        }
+        //static void Main(string[] args)
+        //{
+        //    InsertRow("Left4Dead2", 2012, 2000);
+        //    SelectAllRows();
+        //}
     }
 }
